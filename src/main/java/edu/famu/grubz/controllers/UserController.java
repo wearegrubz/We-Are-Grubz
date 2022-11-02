68fc2f4fc554b20967e62703e65170633ba80470
Kenneth Rice <kenneth1.rice1@gmail.com>
2022-11-02 12:00:57 -0400

    adding retrieve in user service and controller

--- src/main/java/edu/famu/grubz/controllers/UserController.java
+++ src/main/java/edu/famu/grubz/controllers/UserController.java
@@ -1,4 +1,34 @@
 package edu.famu.grubz.controllers;
 
+import edu.famu.grubz.models.parse.User;
+import edu.famu.grubz.models.serializable.SerializableUser;
+import edu.famu.grubz.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = {"/"}) //sets the path to this method
+    public ArrayList<SerializableUser> getUserList() {
+        ArrayList<SerializableUser> users = new ArrayList<>();
+
+        //Convert the Parse Product object to a POJO Product object that can be serialized by Spring
+        ArrayList<User> list = userService.retrieveUsers();
+        for(User p : list)
+        {
+            users.add(p.getSerializable());
+        }
+        return users;
+    }
 }
--- src/main/java/edu/famu/grubz/services/UserService.java
+++ src/main/java/edu/famu/grubz/services/UserService.java
@@ -1,4 +1,39 @@
 package edu.famu.grubz.services;
 
+import edu.famu.grubz.models.parse.User;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.parse4j.Parse;
+import org.parse4j.ParseQuery;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserService {
+
+    protected final Log logger = LogFactory.getLog(this.getClass()); //used to write to the console
+
+    public ArrayList<User> retrieveUsers()
+    {
+        logger.info(Parse.isIsRootMode());
+        final ArrayList<User> users = new ArrayList<>();
+
+        ParseQuery<User> query = ParseQuery.getQuery(User.class);
+
+        try {
+            List<User> list = query.find();
+            for (User p : list) {
+                logger.info(p.toString()); //use if you want to see your products in the console
+                users.add(p);
+            }
+        }
+        catch(Exception e)
+        {
+            logger.error("Error occurred", e);
+        }
+        logger.info(users.size());
+        return users;
+    }
 }
