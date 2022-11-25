1cbae8690859010423ee0a8d5a1f90f2c5c3b0ea
Kenneth Rice <kenneth1.rice1@gmail.com>
2022-11-25 04:43:24 -0500

    fixing bug in react config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,12 +11,9 @@ import Navbar from "./components/Navbar";
 import SelectTaste from "./pages/SelectTaste";
 import CreateGroup from "./pages/CreateGroup";
 import SignUp from "./pages/SignUp";
-/*
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const Parse = require('parse/react-native.js');
-
-
-
 Parse.setAsyncStorage(AsyncStorage);
 
 const PARSE_APPLICATION_ID = process.env.PARSE_APPLICATION_ID;
@@ -24,7 +21,7 @@ const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
 const PARSE_JAVASCRIPT_ID = process.env.PARSE_JAVASCRIPT_ID;
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_ID);
 Parse.serverURL = PARSE_HOST_URL;
-*/
+
 
 function App() {
   return (
